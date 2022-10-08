73b3ef35fed773aaf7d3cd3c67c82c25a352b327
SourabhaDixit <70750867+SourabhaDixit@users.noreply.github.com>
2022-10-08 08:20:27 +0530

    "second commit"

--- src/test/java/testng/HRMLoginApplyLeaveOverload.java
+++ src/test/java/testng/HRMLoginApplyLeaveOverload.java
@@ -11,7 +11,7 @@ import org.testng.annotations.Test;
 public class HRMLoginApplyLeaveOverload {
 	private static WebDriver driver = null;
 	//Login 
-  @Test
+  @Test(priority = 1)
   public void login() {
 	  System.setProperty("webdriver.chrome.driver", "C:\\Sourabha\\chromedriver.exe");
 	    driver = new ChromeDriver();
@@ -22,15 +22,9 @@ public class HRMLoginApplyLeaveOverload {
 		driver.findElement(By.xpath("//button[@type='submit']")).click();
  }
   //Apply leave
-  @Test
+  @Test(priority = 2)
   public void hrmleave() {
-	  System.setProperty("webdriver.chrome.driver", "C:\\Sourabha\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-		driver.manage().timeouts().implicitlyWait(10000, TimeUnit.MILLISECONDS);
-		driver.findElement(By.cssSelector("input[@name = 'username']")).sendKeys("Admin");
-		driver.findElement(By.xpath("//input[@type = 'password']")).sendKeys("admin123");
-		driver.findElement(By.xpath("//button[@type = 'submit']")).click();
+
 		driver.findElement(By.linkText("Leave")).click();
 		driver.findElement(By.linkText("Apply")).click();
 		driver.findElement(By.xpath("//div[@class='oxd-select-text oxd-select-text--active']/div[2]/i")).click();
@@ -40,7 +34,7 @@ public class HRMLoginApplyLeaveOverload {
   }
   // Method overload for search function
   //data provider which provides one attribute
-  @Test(dataProvider = "SearchByUsername" )
+  @Test(dataProvider = "SearchByUsername", priority = 3 )
 	 public void search(String username){
 	  	System.setProperty("webdriver.chrome.driver", "C:\\Sourabha\\chromedriver.exe");
 	    driver = new ChromeDriver();
@@ -55,7 +49,7 @@ public class HRMLoginApplyLeaveOverload {
 	
 	 }	
 //data provider which provides two attribute
-  @Test(dataProvider = "SearchByUsernameEmpName" )
+  @Test(dataProvider = "SearchByUsernameEmpName" ,priority = 4 )
 	public void search(String username, String empName) {
 	  System.setProperty("webdriver.chrome.driver", "C:\\Sourabha\\chromedriver.exe");
 	    driver = new ChromeDriver();
